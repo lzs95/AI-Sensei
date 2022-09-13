6f3ed3d7b7f4d2a13fe6e73820ca79ea53ca85f8
lzs95 <86816779+lzs95@users.noreply.github.com>
2022-09-13 20:07:03 +0900

    Feat addition of back button for result

--- aisensei-fe/pages/results.tsx
+++ aisensei-fe/pages/results.tsx
@@ -3,15 +3,20 @@ import React from "react";
 interface ResultProps {
   words: string[];
   userInputText: string;
+  onReturn: any;
 }
 
 export const Results: React.FC<ResultProps> = (props) => {
   console.log(props.userInputText);
   console.log(props.words);
+
   return (
     <div className="flex flex-col items-center">
       <p>{props.userInputText}:</p>
       <p>{props.words.join(" ,")}</p>
+      <button className=" bg-orange-600 w-52" onClick={props.onReturn}>
+        back
+      </button>
     </div>
   );
 };
